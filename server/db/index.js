3d9072503c8dea95744a8165513bdb4e25cc4d75
Simon.Burton <si_burton@hotmail.com>
2022-07-22 15:20:03 +0100

    Added: List both parents for any given family member List children for any given family member

--- server/db/index.js
+++ server/db/index.js
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
 
 let persondb = {};
 
+//localhost:3000/api/test
 persondb.all = () => {
    return new Promise((resolve, reject) => {
      pool.query(`SELECT * FROM familyTree`,(err,results) => {
@@ -21,6 +22,42 @@ persondb.all = () => {
         return resolve(results);
       });
    });
+};
+
+persondb.person = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM familyTree WHERE id = ?`,[id],(err,results) => {
+            if(err){
+                return reject(err);
+            }
+            return resolve(results[0]);
+        });
+    });
+};
+
+//Return parents based on persons ID
+persondb.parents = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT forename,surname FROM familyTree WHERE id = (SELECT motherId FROM familyTree WHERE id = ?) OR id = (SELECT fatherId FROM familyTree WHERE id = ?)`,[id,id],(err,results) => {
+            if(err){
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+//Return parents based on persons ID
+//id = (SELECT id FROM familyTree WHERE motherId = ?) OR
+persondb.children = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM familyTree WHERE id in (SELECT id FROM familyTree WHERE motherId = ?) OR id in (SELECT id FROM familyTree WHERE fatherId = ?)`,[id,id],(err,results) => {
+            if(err){
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
 };
 
 module.exports = persondb;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const db = require('../db');
 const router = express.Router();
 
-router.get('/',async (req,res,next) => {
+//Return all people
+router.get('/id',async (req,res) => {
     //res.json({test:`test`});
     try{
         let results = await db.all();
@@ -12,8 +13,48 @@ router.get('/',async (req,res,next) => {
         console.log(e);
         res.sendStatus(500);
     }
-
 });
+
+//Return person by id
+router.get('/id/:id',async (req,res) => {
+    //res.json({test:`test`});
+    try{
+        let results = await db.person(req.params.id);
+        res.json(results);
+    }catch (e)
+    {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+//Return parents by person id
+router.get('/parents/:id',async (req,res) => {
+    //res.json({test:`test`});
+    try{
+        let results = await db.parents(req.params.id);
+        res.json(results);
+    }catch (e)
+    {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+//Return children by parent id
+router.get('/children/:id',async (req,res) => {
+    //res.json({test:`test`});
+    try{
+        let results = await db.children(req.params.id);
+        res.json(results);
+    }catch (e)
+    {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+
 
 module.exports = router;
 
